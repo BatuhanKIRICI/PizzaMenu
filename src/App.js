96c062d28d1c245719fcf392247ff3e9d9e56917
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-14 13:05:44 +0200

    "Conditional rendering with ternaries"

--- src/App.js
+++ src/App.js
@@ -65,18 +65,21 @@ function Header() {
 }
 
 function Menu() {
-  const pizzas = [];
+  const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza, index) => (
             <Pizza key={index} pizzaObj={pizza} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on in our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -103,11 +106,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order Now!</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
